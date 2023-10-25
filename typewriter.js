c64892f3b06ef01db3024df20b145841b51ad78a
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-25 19:33:04 -0400

    first async file

---
+++ typewriter.js
@@ -0,0 +1,38 @@
+const sentence = "hello there";
+
+  setTimeout(() => {
+    process.stdout.write('H');
+  }, 0) 
+  setTimeout(() => {
+    process.stdout.write('e');
+  }, 50)
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 100)
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 150)
+  setTimeout(() => {
+    process.stdout.write('o');
+  }, 200)
+  setTimeout(() => {
+    process.stdout.write(' ');
+  }, 250)
+  setTimeout(() => {
+    process.stdout.write('t');
+  }, 300)
+  setTimeout(() => {
+    process.stdout.write('h');
+  }, 350)
+  setTimeout(() => {
+    process.stdout.write('e');
+  }, 400)
+  setTimeout(() => {
+    process.stdout.write('r');
+  }, 450)
+  setTimeout(() => {
+    process.stdout.write('e');
+  }, 500)
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, 600)
